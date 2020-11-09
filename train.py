9523f0127a98a2a28bc1efcd915a0cbd4dc9ef47
Dina Abdul-Rasoul <73671062+dinaabdulrasoul@users.noreply.github.com>
2020-11-09 00:08:10 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -9,33 +9,6 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
-
-# TODO: Create TabularDataset using TabularDatasetFactory
-
-
-
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-### YOUR CODE HERE ###
-import azureml.core import Dataset
-path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-ds=Dataset.Tabular.from_delimited_files(path=[(datastore, path)]
-
-
-
-x, y = clean_data(ds)    #
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###
-from sklearn.model_selection import train_test_split  
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)                                        
-       
-
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -61,7 +34,36 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_df,y_df                                    
     
+
+
+# TODO: Create TabularDataset using TabularDatasetFactory
+
+
+
+# Data is located at:
+# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+### YOUR CODE HERE ###
+path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds=TabularDatasetFactory.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
+
+
+
+x, y = clean_data(ds)    #
+
+# TODO: Split data into train and test sets.
+
+### YOUR CODE HERE ###
+from sklearn.model_selection import train_test_split  
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)                                        
+       
+
+
+run = Run.get_context()
+
+
 
 def main():
     # Add arguments to script
